a3ba8f1ef6c54bfe4a2573cb63c09b6b71e283ad
Maxislav <lmaxim@mail.ru>
2015-01-03 22:13:33 +0200

    ревормат 1

--- module/directive/directive-calendar.js
+++ module/directive/directive-calendar.js
@@ -1,91 +1,97 @@
-calendarModule.directive('calendar', ['$compile', '$templateCache','constantCalendar', function ($compile, $templateCache, constantCalendar) {
-    function formatMonth(date){
+calendarModule.directive('calendar', ['$compile', '$templateCache', 'constantCalendar', function ($compile, $templateCache, constantCalendar) {
+    function formatMonth(date, _selectDate) {
         var dateNowValue = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime();
-        function checkCurrentDate(d){
-            if(dateNowValue == d.getTime()){
+        var selectDate = new Date(_selectDate.getFullYear(), _selectDate.getMonth(), _selectDate.getDate()).getTime();
+
+        function checkCurrentDate(d) {
+            if (dateNowValue == d.getTime()) {
                 return true
             }
             return false
         }
+
+
+
         var array = [];
-        for (var i = 0; i<50; i++){
-            var d = new Date(date.getFullYear(), date.getMonth(),i)
-            if(d.getMonth() == date.getMonth() ){
+        for (var i = 0; i < 50; i++) {
+            var d = new Date(date.getFullYear(), date.getMonth(), i)
+            if (d.getMonth() == date.getMonth()) {
                 array.push({
                     date: d,
                     value: d.getTime(),
                     dayWeek: d.getDay(),
-                    class: checkCurrentDate(d) ? "current" :''
+                    class: checkCurrentDate(d) ? "current" : ''
                 })
             }
         }
-        //console.log(array);
-        var fillBefore = (array[0].dayWeek == 0) ? 6 : array[0].dayWeek-1;
-        var fillAfter = 7 - array[array.length-1].dayWeek;
-        for(var i = 0; i<fillBefore; i++){
+
+        var fillBefore = (array[0].dayWeek == 0) ? 6 : array[0].dayWeek - 1;
+        var fillAfter = 7 - array[array.length - 1].dayWeek;
+        for (var i = 0; i < fillBefore; i++) {
             array.unshift(null)
         }
-        for(var i = 0; i<fillAfter; i++){
+        for (var i = 0; i < fillAfter; i++) {
             array.push(null)
         }
 
-        var rows = parseFloat((array.length/7).toFixed(0));
+        var rows = parseFloat((array.length / 7).toFixed(0));
         var arrMonth = [];
         var k = 0
-        for(var i = 0; i<rows;i++){
-            arrMonth[i]=[];
-            for(var d = 0; d<7;d++){
+        for (var i = 0; i < rows; i++) {
+            arrMonth[i] = [];
+            for (var d = 0; d < 7; d++) {
                 arrMonth[i].push(array[k])
                 k++;
             }
 
         }
-      //  console.log(arrMonth)
         return arrMonth
     }
-
 
     return {
         restrict: 'EA',
         replace: true,
         templateUrl: 'module/partials/calendar-label.html',
         scope: {
-            before: '=before',
-            after: '=after'
+            date: '=date'
         },
         controller: ['$scope', '$element', '$attrs', '$timeout', '$templateCache', function ($scope, $element, $attrs, $timeout, $templateCache) {
             var content = null;
             $scope.show = false;
             $scope.constantCalendar = constantCalendar;
-            var date = new Date();
-            //var currentMonth = date.getMonth()
+            var dateCalendar = $scope.date
+            var viewMonth =  $scope.date.getMonth();
 
+            $scope.arrMonth = formatMonth(dateCalendar, $scope.date);
+            $scope.arrMonth.value = dateCalendar.getTime();
 
-            $scope.arrMonth = formatMonth(date);
-            $scope.arrMonth.value = date.getTime();
+            $scope.stepBack = function(){
+                viewMonth--
+                regen(viewMonth)
+            }
+            $scope.stepForward = function(){
+                viewMonth++
+                regen(viewMonth)
+            }
 
-            var dateBefore = new Date(date.getFullYear(),date.getMonth()-1,1)
-            $scope.arrMonthBefore = formatMonth(dateBefore)
-            $scope.arrMonthBefore.value = dateBefore.getTime()
-
-            var dateAfter = new Date(date.getFullYear(),date.getMonth()+1,1)
-            $scope.arrMonthAfter = formatMonth(dateAfter)
-            $scope.arrMonthAfter.value = dateAfter.getTime()
+            function regen(month){
+                var date = new Date($scope.date.getFullYear(), month, $scope.date.getDate())
+                $scope.arrMonth = formatMonth(date,  $scope.date);
+                $scope.arrMonth.value = date.getTime();
+            }
 
             function init() {
                 var linkFn = $compile($templateCache.get('module/partials/calendar-view.html'));
                 content = linkFn($scope);
                 $element.append(content);
             }
+
             $scope.click = function () {
                 if (!content) {
                     init()
                 }
                 $scope.show = !$scope.show
             }
-            console.log($scope.before)
-
-
         }]
     }
 }])
--- src/controllers/default-contrl.js
+++ src/controllers/default-contrl.js
@@ -1,8 +1,6 @@
 app.controller('defaultContrl', ['$scope', '$interval',function($scope, $interval){
-	var date = new Date();
-	$scope.after = new Date(date.getFullYear(), date.getMonth(), date.getDate()-4).getTime();
-	$scope.before = new Date().getTime();
-	$interval(function(){
+	$scope.date = new Date(new Date().getFullYear(), new Date().getMonth()-4, new Date().getDate());
+	/*$interval(function(){
 		$scope.before = new Date().getTime();
-	},1000)
+	},1000)*/
 }])
